66a76d09d319a7ca518e00e807f94296495f66cd
lovleenkaur <lovleenk99@gmail.com>
2023-06-20 14:39:40 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(nameArray, event){
+    let thankYourCard = [];
+    for (let i = 0; i < nameArray.length; i++) { 
+        thankYourCard.push(`Thank you, ${nameArray[i]}, for the wonderful surprise gift!`)
+    }
+    return thankYourCard
+}
+
+function countDown(startingNumber){ 
+    while(startingNumber > 0) { 
+        console.log(startingNumber); 
+        startingNumber -=1;
+    }
+    console.log(startingNumber)
+}
